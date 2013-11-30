fdffa8108fc9da9b1e30bbd15afcae15db6b8e52
MMonedix <mmonedix@gmail.com>
2013-11-30 07:15:31 +0200

    not initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chili::Application.config.secret_token = '2bbf9a1a6288c6c0df39a1222bcce8f6d61c1861510bae3dcbfbeade49cbf55984e156405dfe8194b2aeab493f26b218806309d97724478a17efb4750013a469'
